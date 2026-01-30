4e4ebcc908972d9604527017ae24cf8a2bf482e1
Tahreem Shaikh <98979016+Trying-tostudy-byte@users.noreply.github.com>
2026-01-31 04:05:01 +0530

    Create multiqc.sh for FastQC report aggregation
    Add script to aggregate FastQC reports using MultiQC

--- scripts/qc/multiqc.sh
+++ scripts/qc/multiqc.sh
@@ -1,1 +1,9 @@
 
+# Aggregating FastQC reports into a single summary using MultiQC
+
+# Creatin output directory
+mkdir -p results/qc_reports/multiqc
+
+# Generating MultiQC report
+multiqc results/qc_reports/fastqc \
+  -o results/qc_reports/multiqc
