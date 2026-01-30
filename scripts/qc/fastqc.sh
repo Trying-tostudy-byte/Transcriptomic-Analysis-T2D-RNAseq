8609ab96c4d6502f6337ce97b90ebb18a5e9ba4f
Tahreem Shaikh <98979016+Trying-tostudy-byte@users.noreply.github.com>
2026-01-31 04:03:49 +0530

    Enhance fastqc.sh with comments and output directory
    Added comments for clarity and created output directory.

--- scripts/qc/fastqc.sh
+++ scripts/qc/fastqc.sh
@@ -1,7 +1,9 @@
 
+# Performing quality control on raw RNA-seq FASTQ files using FastQC
 
+# Creating output directory 
 mkdir -p results/qc_reports/fastqc
 
+# Running FastQC on all raw FASTQ files
 fastqc data/raw/*.fastq.gz \
   -o results/qc_reports/fastqc
-
